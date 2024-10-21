dce5d775db2d363ed634f19a54882921fbf0845b
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2024-10-21 22:32:20 +0700

    updated at 10-21-2024

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const baseURL = "https://api.api-ninjas.com/v1/quotes";
 class Http {
   get(url) {
     return fetch(url, {
-      method: 'GET',
-      headers: {'X-Api-Key': 'OGl8kMqbLvD78DEngjiKbw==tzLo7DiAQAPXJMYs'},
+      method: "GET",
+      headers: { "X-Api-Key": "OGl8kMqbLvD78DEngjiKbw==tzLo7DiAQAPXJMYs" },
     }).then((response) => {
-        if(response.ok){
-            return response.json();
-        }else{
-            throw new Error(response.statusText)
-        }
-    })
-  } 
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    });
+  }
 }
-//Class Store dùng để get nội dung đầy đủ bao gồm Quote, Author, Category 
+//Class Store dùng để get nội dung đầy đủ bao gồm Quote, Author, Category
 //Nhưng do hiện tại tôi chỉ hiển thị quote và author nên sẽ không dùng category <3
 class Store {
   //tạo instance của Http
@@ -26,12 +26,12 @@ class Store {
   getRandomQuote() {
     return this.http.get(`${baseURL}`);
   }
-};
+}
 //Class RenderUI dùng để hiển thị nội dung lên UI(không hiển thị category (rảnh sẽ làm))
 class RenderUI {
   renderRandomQuote(quoteInfor) {
-    const quote = (quoteInfor[0])["quote"];
-    const author = (quoteInfor[0])["author"];
+    const quote = quoteInfor[0]["quote"];
+    const author = quoteInfor[0]["author"];
     let htmlContent = `
         <h2 class="h2_title">Quote of the day</h2>
         <div class="block__quote">
@@ -41,10 +41,10 @@ class RenderUI {
             <span class="span_author" id="author">${author}</span>
         </div>
         `;
-    
-        //chỉ thay thế nội dung của quote và author 
-        document.querySelector("#quote").textContent = quote;
-        document.querySelector("#author").textContent = author;
+
+    //chỉ thay thế nội dung của quote và author
+    document.querySelector("#quote").textContent = quote;
+    document.querySelector("#author").textContent = author;
   }
 }
 //------------------------------------------MAIN EVENT---------------------------------------------------
@@ -55,7 +55,6 @@ document.querySelector("#newQuote").addEventListener("click", async (event) => {
   //Nhận data và hiển thị lên UI
   let randomQuote = await store.getRandomQuote();
   ui.renderRandomQuote(randomQuote);
-
 });
 
 //Còn sự kiện bấm vào nút Facebook
