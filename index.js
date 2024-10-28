dcc5ab15bd251977b7f766217af646f2e76bc256
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2024-10-28 18:18:39 +0700

    updated 2024-28-10

--- index.js
+++ index.js
@@ -21,10 +21,13 @@ class Store {
   //tạo instance của Http
   constructor() {
     this.http = new Http();
+    //lưu giá trị hiện tại
+    this.currentQuote = null;
   }
   //Tạo hàm getRandomQuote
-  getRandomQuote() {
-    return this.http.get(`${baseURL}`);
+  async getRandomQuote() {
+    this.currentQuote = await this.http.get(`${baseURL}`);
+    return this.currentQuote;
   }
 }
 //Class RenderUI dùng để hiển thị nội dung lên UI(không hiển thị category (rảnh sẽ làm))
@@ -46,19 +49,31 @@ class RenderUI {
     document.querySelector("#quote").textContent = quote;
     document.querySelector("#author").textContent = author;
   }
+  renderTweet(quoteInfor) {
+    const quote = quoteInfor[0]["quote"];
+    const author = quoteInfor[0]["author"];
+
+    window.open(
+      `https://twitter.com/intent/tweet?text=${quote} - ${author}`,
+      "Tweet Window",
+      "width=600, height=300"
+    );
+    document.querySelector("#quote").textContent;
+    document.querySelector("#author").textContent;
+  }
 }
 
 //hàm khởi tạo câu đạo lí của mình
-const tweet = () => {
-  const quote = document.querySelector("#quote").textContent;
-  const author = document.querySelector("#author").textContent;
+// const tweet = () => {
+//   const quote = document.querySelector("#quote").textContent;
+//   const author = document.querySelector("#author").textContent;
 
-  window.open(
-    `https://twitter.com/intent/tweet?text=${quote} - ${author}`,
-    "Tweet Window",
-    "width=600, height=300"
-  );
-};
+//   window.open(
+//     `https://twitter.com/intent/tweet?text=${quote} - ${author}`,
+//     "Tweet Window",
+//     "width=600, height=300"
+//   );
+// };
 //------------------------------------------MAIN EVENT---------------------------------------------------
 document.querySelector("#newQuote").addEventListener("click", async (event) => {
   //tạo instance của Store và RenderUI
@@ -67,8 +82,19 @@ document.querySelector("#newQuote").addEventListener("click", async (event) => {
   //Nhận data và hiển thị lên UI
   let randomQuote = await store.getRandomQuote();
   ui.renderRandomQuote(randomQuote);
+
+  //lưu giá trị quote lại cho Link
+  document.querySelector("#Link").dataset.quote = JSON.stringify(randomQuote);
 });
 
-//Còn sự kiện bấm vào nút Facebook
+document.querySelector("#Link").addEventListener("click", async (event) => {
+  const ui = new RenderUI();
+  const quoteData = document.querySelector("#Link").dataset.quote;
+  if (quoteData) {
+    ui.renderTweet(JSON.parse(quoteData));
+  } else {
+    alert("Đu Ma May Bấm New Quote rồi mới coi tao đạo lí được ");
+  }
+});
 //chưa README
-//chưa Deloy lêm
+//chưa Deloy lên
